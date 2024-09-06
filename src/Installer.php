a5b6649cc0b1b78b3dacadbb5b7737e0dab9f8b6
RobertoRomolini <63667868+RobertoRomolini@users.noreply.github.com>
2024-09-06 12:54:53 +0200

    Fix installer and update ReadMe (#3)
    * Update readme
    
    * Fix installer
    
    ---------
    
    Co-authored-by: Roberto Romolini <r.romolini@sintra.eu>

--- src/Installer.php
+++ src/Installer.php
@@ -2,15 +2,49 @@
 
 namespace SyncShopifyBundle;
 
-use Pimcore\Extension\Bundle\Installer\AbstractInstaller;
+use Doctrine\DBAL\Connection;
+use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
 use Pimcore\Model\DataObject\ClassDefinition;
 use Pimcore\Model\DataObject\ClassDefinition\Service;
+use Pimcore\Model\DataObject\Fieldcollection\Definition;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
-class Installer extends AbstractInstaller
+class Installer extends SettingsStoreAwareInstaller
 {
     const DEFAULT_PRODUCT_FILE = __DIR__ . "/../config/install/class_DefaultProduct.json";
+    const IMAGE_INFO_FILE = __DIR__ . "/../config/install/fieldcollection_ImageInfo.json";
+
+    public function __construct(
+        protected BundleInterface $bundle,
+        protected Connection      $db
+    )
+    {
+        parent::__construct($bundle);
+    }
 
     public function install(): void
+    {
+        $this->installFieldCollections();
+
+        $this->installDataObjects();
+
+        parent::install();
+    }
+
+    private function installFieldCollections(): void
+    {
+        $fieldCollection = Definition::getByKey("ImageInfo");
+        if (empty($fieldCollection)) {
+            $fieldCollection = new Definition();
+            $fieldCollection->setKey("ImageInfo");
+
+            $json = file_get_contents(self::IMAGE_INFO_FILE);
+
+            Service::importFieldCollectionFromJson($fieldCollection, $json);
+        }
+    }
+
+    private function installDataObjects(): void
     {
         $class = ClassDefinition::getById("DEFAULT_PROD");
         if (empty($class)) {
@@ -22,19 +56,5 @@ class Installer extends AbstractInstaller
 
             Service::importClassDefinitionFromJson($class, $json);
         }
-    }
-
-    public function uninstall(): void
-    {
-    }
-
-    public function canBeInstalled(): bool
-    {
-        return true;
-    }
-
-    public function canBeUninstalled(): bool
-    {
-        return true;
     }
 }
